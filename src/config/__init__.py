9a89280eb43680dd1d62a3dd9353000fae834483
id-shiv <id.shiv.prasad@gmail.com>
2020-09-19 22:26:51 +0530

    get input elements module update

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -3,4 +3,4 @@ PROJECT_PATH = '/Users/shiv/Documents/gitRepositories/itest/'
 URL = "https://demoqa.com/automation-practice-form"
 
 # constansts
-DRIVER_PATH = PROJECT_PATH + '/src/scraper/ui/drivers/chromedriver-5'
+DRIVER_PATH = PROJECT_PATH + 'src/ui/drivers/chromedriver-5'
--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -6,9 +6,17 @@ import numpy as np
 
 # Insert source package path to import modules from config
 import sys
-sys.path.insert(0, sys.path[0].split('src')[0] + 'src')
+src_path = sys.path[0].split('itest')[0] + 'itest/src'
+print(f'inserting source path {src_path}')
+sys.path.insert(0, src_path)
+print(f'sys path after insert: {sys.path}')
 
 from config import DRIVER_PATH
+
+# constants
+TAG_ATTRIBUTES = ['//*[@id]', '//*[@class]', '//*[@name]']
+INPUT_TAGS = ['input', 'textarea', 'button']
+
 
 def get_driver(browser: str='chrome'):
     try:
@@ -37,31 +45,45 @@ def launch(url: str, driver: webdriver):
         print(f'Failed to launch {url}')
         return None
 
-if __name__ == "__main__":
-    from config import URL
+def get_input_elements(url: str):
     driver = get_driver()
-    launch(URL, driver)
+    driver = launch(URL, driver)
 
-    inputs = list()  # to contain all input elements
+    input_elements = list()  # to contain all input elements
 
-    tag_types = ['//*[@id]', '//*[@class]', '//*[@name]']
-    for tag_type in tag_types:
+    for tag_type in TAG_ATTRIBUTES:
         # get only text from tag. e.g. id, class, name
         tag_type_text = tag_type.split('[')[1].split(']')[0][1:]
 
+        # find all elements with specific tag type
         tags = driver.find_elements_by_xpath(tag_type)
         for tag in tags:
-            if tag.tag_name in ['input', 'textarea', 'button']:
+            if tag.tag_name in INPUT_TAGS:
                 _input = dict()
-                # _input['tag_type'] = tag_type_text
+                
+                # collect all the input details, i.e. id, class, name and value
                 _input['tag_id'] = tag.get_attribute('id')
                 _input['tag_class'] = tag.get_attribute('class')
                 _input['tag_name'] = tag.get_attribute('name')
+                _input['tag_value'] = tag.get_attribute('value')
+                _input['tag_type'] = tag.get_attribute('type')
                 _input['tag_text'] = tag.text
-                inputs.append(_input)
+                input_elements.append(_input)
     
-    inputs=list(np.unique(np.array(inputs).astype(str)))
+    # filter only the unique tag details in the list
+    input_elements = list(np.unique(np.array(input_elements).astype(str)))
 
-    for _input in inputs:
-        print(_input)
+    # close the browser
     driver.close()
+
+    return input_elements
+
+if __name__ == "__main__":
+    # test above methods
+    from config import URL
+    
+    # get input elements
+    input_elements = get_input_elements(url=URL)
+    for element in input_elements:
+        print(element)
+    
